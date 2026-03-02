1c9cb6e604f3e2b6ad061d74ab096d785d85c6a9
dario bertonati <dario.bertonatilavoro@gmail.com>
2026-03-03 00:47:43 +0100

    updated domain name

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
                 <main id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
                     <?php the_content(); ?>
-                    <?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'mytheme' ), 'after' => '</div>', ) ); ?>
+                    <?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'Soptis' ), 'after' => '</div>', ) ); ?>
                     
                 </main>
             <?php endwhile; ?>
