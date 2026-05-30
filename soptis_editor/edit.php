b0a3f0c86129b12cdf481d83d32e9880f0756dc0
dario bertonati <dario.bertonatilavoro@gmail.com>
2026-05-30 19:57:11 +0200

    updated plugin

--- soptis_editor/edit.php
+++ soptis_editor/edit.php
@@ -1,17 +1,18 @@
-/*
- * Plugin Name:       Soptis_editor
- * Plugin URI:        https://github.com/DBDeve/Soptis
- * Description:       This plugin modifies the wordpress editor.
- * Version:           1.0.0
- * Requires at least: 5.2
- * Requires PHP:      7.2
- * Author:            DBDeve
- * Author URI:        www.linkedin.com/in/dbdeve
- * License:           GPL v2 or later
- * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- */
-
 <?php
+
+    /*
+    * Plugin Name:       Soptis_editor
+    * Plugin URI:        https://github.com/DBDeve/Soptis
+    * Description:       This plugin modifies the wordpress editor.
+    * Version:           1.0.0
+    * Requires at least: 5.2
+    * Requires PHP:      7.2
+    * Author:            DBDeve
+    * Author URI:        www.linkedin.com/in/dbdeve
+    * License:           GPL v2 or later
+    * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+    */
+
 
     add_action( 'enqueue_block_editor_assets', function() {
         wp_enqueue_style(
